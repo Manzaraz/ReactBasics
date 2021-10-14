5dfea62197e6217431505138c39b68e95a37e0fa
Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2021-10-14 16:35:04 -0300

    peticiones asincronas con ajax

--- src/components/AjaxApis.js
+++ src/components/AjaxApis.js
@@ -23,12 +23,12 @@ export default class AjaxApi extends Component {
         fetch(url)
             .then(res => res.json())
             .then(json => {
-                console.log(json)
+                // console.log(json)
                 json.results.forEach(el => {
                     fetch(el.url)
                         .then(res => res.json())
                         .then(json => {
-                            console.log(json)
+                            // console.log(json)
                             let pokemon = {
                                 id: json.id,
                                 name: json.name,
@@ -36,9 +36,9 @@ export default class AjaxApi extends Component {
                             };
                             let pokemons = [...this.state.pokemons, pokemon];
                             this.setState({ pokemons });
-                        })
-                })
-            })
+                        });
+                });
+            });
     };
 
     render() {
--- src/components/CicloVida.js
+++ src/components/CicloVida.js
@@ -1,12 +1,12 @@
 import React, {Component} from "react";
 
 class Reloj extends Component {
-    constructor(props) {
-        super(props)
-        console.log("hola")
-    }
+    // constructor(props) {
+    //     super(props)
+    //     // console.log("hola")
+    // }
     componentWillUnmount(){
-        console.log(3, "El componente ha sido eliminado del DOM");
+        // console.log(3, "El componente ha sido eliminado del DOM");
     }
     render() {
         return (
@@ -20,7 +20,7 @@ class Reloj extends Component {
 export default class CicloVida extends Component {
     constructor(props) {
         super(props);
-        console.log(0, "El componente se inicializa, aún NO está en el DOM")
+        // console.log(0, "El componente se inicializa, aún NO está en el DOM")
 
         this.state = {
             hora: new Date().toLocaleTimeString(),
@@ -52,18 +52,18 @@ export default class CicloVida extends Component {
     }
 
     componentDidMount() {
-        console.log(1, "El componente ya se encuentra en el DOM");
+        // console.log(1, "El componente ya se encuentra en el DOM");
     }
 
     componentDidUpdate(prevProps, prevState) {
-        console.log(2, "El estado o las props del componente han cambiado");
-        console.log(prevProps)
-        console.log(prevState)
+        // console.log(2, "El estado o las props del componente han cambiado");
+        // console.log(prevProps)
+        // console.log(prevState)
     }
 
 
     render() {
-        console.log(4, "El componente se dibuja (o se redibuja) por un cambio en el DOM")
+        // console.log(4, "El componente se dibuja (o se redibuja) por un cambio en el DOM")
         return (
             <>
                 <h2>Ciclo de vida de los Componentes de Clase</h2>
